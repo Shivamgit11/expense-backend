b0ba2ce59defee5ccee777a03723fd71ca159ca1
shivam <mailtoshivam2002@gmail.com>
2023-02-23 20:25:51 +0530

    done some changes in backend and cleanup code

--- app.js
+++ app.js
@@ -4,15 +4,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const sequelize = require("./util/database");
 
-const errorController = require("./controllers/error");
+
 
 const app = express();
 
 // app.set("view engine", "ejs");
 // app.set("views", "views");
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
 const userRoutes = require("./routes/user");
 const expeneRoutes = require("./routes/expense");
 const User = require("./models/User");
@@ -24,22 +22,19 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
-
 app.use("/user", userRoutes);
+
+//expense
 app.use("/expense", expeneRoutes);
 
 //working for expense backend
 
-
-
 ///everty thing for expense will be done inside it
 
-app.use(errorController.get404);
+
 
 sequelize
-  .sync({ alter: true })
+  .sync({ alter: false, force: true })
   .then((result) => {
     // console.log(result);
     app.listen(3000);
