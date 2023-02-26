4dbebd3712131f57991775ee97c974cce70d06a5
shivam <mailtoshivam2002@gmail.com>
2023-02-26 23:02:20 +0530

    doing some changes for backend

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,24 +1,42 @@
 const Auth = require("../models/Auth");
 
+function isstringinvalid(string) {
+  if (string === undefined || string.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 const addAuth = async (req, res, next) => {
   console.log(req);
-    try {
-      const name = req.body.name;
-      const email = req.body.email;
-      const password = req.body.password;
-      const data = await Auth.create({
-        name: name,
-        email: email,
-        password: password,
-      });
-      res.status(201).json({ newAuthdetails: data });
-    } catch (err) {
-      res.status(500).json({
-        error: err,
-      });
-    }
-  };
+  try {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
 
-  module.exports = {
-    addAuth,
+    if (
+      isstringinvalid(name) ||
+      isstringinvalid(email) ||
+      isstringinvalid(password)
+    ) {
+      return res
+        .status(400)
+        .json({ err: "Bad parameters, something is missing" });
+    }
+
+    await Auth.create({
+      name: name,
+      email: email,
+      password: password,
+    });
+    res.status(201).json({ message: "Successfully created new user" });
+  } catch (err) {
+    res.status(500).json({
+      error: err,
+    });
   }
+};
+
+module.exports = {
+  addAuth,
+};
